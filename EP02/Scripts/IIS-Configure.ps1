7653a72b5b215110598df316ff91edcb79134f3f
William <william-crcosta@outlook.com>
2025-04-09 22:57:38 -0300

    Include folders on gitignore file + IISConfigure

--- EP02/Scripts/IIS-Configure.ps1
+++ EP02/Scripts/IIS-Configure.ps1
@@ -1,17 +1,11 @@
-
 # This script installs the IIS web server on a Windows machine.
-# It uses the Install-WindowsFeature cmdlet to install the Web-Server feature along with management tools.
+# It uses the Add-WindowsFeature cmdlet to install the Web-Server feature along with management tools.
 
 # Desativa Firewall Windows
 Set-NetFirewallProfile -Profile Domain, Public, Private -Enabled False
 
 # Install IIS
-​​Install-WindowsFeature -name Web-Server -IncludeManagementTools
-
-
-# Install IIS
-# Add-WindowsFeature -Name Web-Server -IncludeManagementTools
-
+Add-WindowsFeature -Name Web-Server -IncludeManagementTools
 
 # Faz download do Dotnet Hosting copiando para a raiz do disco C.
 Invoke-WebRequest -Uri "https://builds.dotnet.microsoft.com/dotnet/aspnetcore/Runtime/8.0.15/dotnet-hosting-8.0.15-win.exe" -OutFile C:\dotnet-hosting-8.0.15-win.exe;
@@ -43,7 +37,6 @@ Copy-Item -Path "C:\Sistema\" -Destination "C:\inetpub\wwwroot\" -Recurse -Force
 # Stop do Site Default
 Stop-WebSite -Name "Default Web Site"
 
-
 # Remove-Website -Name "Default Web Site"
 
 #Remove-Item -Path IIS:\AppPools\DefaultAppPool
@@ -62,10 +55,5 @@ New-Item –Path IIS:\AppPools\Dash
 #Cra novo Web-Site
 New-Website -Name "Dash" -Port 8080 -PhysicalPath "C:\inetpub\wwwroot\Dash" -ApplicationPool "Dash"
 
-
-
 # Realiza o restart da maquina pós instalação dos componentes.
 Restart-Computer -Force
-# Restart-Computer
-#
-# Restart-Computer -Force
