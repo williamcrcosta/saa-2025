688ce3581d50fcdf02dc53a2c3bb0a65860de1e6
William <william-crcosta@outlook.com>
2025-04-09 23:47:52 -0300

    Include folders on gitignore file + IISConfigure

--- EP02/Scripts/IIS-Configure.ps1
+++ EP02/Scripts/IIS-Configure.ps1
@@ -16,44 +16,5 @@ C:\dotnet-hosting-8.0.15-win.exe /quiet
 # Valida a instalação do Hosting
 dotnet --list-runtimes
 
-#Faz download do pacote das publicações e Banco
-Invoke-WebRequest -Uri "https://stodiskdesafio.blob.core.windows.net/saa2025/Aplicacao.zip" -OutFile C:\Aplicacao.zip;
-
-# Extrai os arquivos e adiciona no diretório wwwroot
-Expand-Archive C:\Aplicacao.zip -DestinationPath C:\
-
-# Copy-Item -Path C:\dash -Destination C:\inetpub\wwwroot\
-Copy-Item -Path "C:\dash\" -Destination "C:\inetpub\wwwroot\" -Recurse -Force
-
-Copy-Item -Path "C:\Sistema\" -Destination "C:\inetpub\wwwroot\" -Recurse -Force
-
-#Copy-Item -Path C:\Sistema -Destination C:\inetpub\wwwroot\
-
-##Expand-Archive C:\Users\Administrator\Downloads\Sistema.zip -DestinationPath C:\inetpub\wwwroot\
-
-# Remove arquivo do Banco de Dados
-##Remove-Item -Path C:\inetpub\wwwroot\Sistema\*.bacpac -Recurse
-
-# Stop do Site Default
-Stop-WebSite -Name "Default Web Site"
-
-# Remove-Website -Name "Default Web Site"
-
-#Remove-Item -Path IIS:\AppPools\DefaultAppPool
-
-# New-WebAppPool -Name Sistema -Force
-
-# Criar Application Pool Sistema
-New-Item –Path IIS:\AppPools\Sistema
-
-# Criar novo Web-Site Sistema
-New-Website -Name "Sistema" -Port 80 -PhysicalPath "C:\inetpub\wwwroot\Sistema" -ApplicationPool "Sistema"
-
-# Criar Application Pool Sistema
-New-Item –Path IIS:\AppPools\Dash
-
-#Cra novo Web-Site
-New-Website -Name "Dash" -Port 8080 -PhysicalPath "C:\inetpub\wwwroot\Dash" -ApplicationPool "Dash"
-
-# Realiza o restart da maquina pós instalação dos componentes.
+Realiza o restart da maquina pós instalação dos componentes.
 Restart-Computer -Force
